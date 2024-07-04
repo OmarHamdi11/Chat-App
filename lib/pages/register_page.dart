e0c30e915f84d7f6113d5555beafe7efd35bca66
OmarHamd11 <omarellafy1@gmail.com>
2024-07-04 04:17:52 +0300

    handel some cases

--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -3,132 +3,148 @@ import 'package:chatapp/widgets/custom_button.dart';
 import 'package:chatapp/widgets/custom_text_field.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
-class RegisterPage extends StatelessWidget {
+class RegisterPage extends StatefulWidget {
   RegisterPage({super.key});
   static String id = 'registerPage';
 
+  @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
   String? email;
+
   String? password;
+
+  bool isloading = false;
 
   GlobalKey<FormState> formKey = GlobalKey();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: kPrimaryColor,
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 8),
-        child: Form(
-          key: formKey,
-          child: ListView(
-            children: [
-              const SizedBox(
-                height: 75,
-              ),
-              Image.asset(
-                'assets/images/scholar.png',
-                height: 100,
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              const Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    'Tech Chat',
-                    style: TextStyle(
-                      fontSize: 32,
-                      color: Colors.white,
-                      fontFamily: 'Pacifico',
+    return ModalProgressHUD(
+      inAsyncCall: isloading,
+      child: Scaffold(
+        backgroundColor: kPrimaryColor,
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8),
+          child: Form(
+            key: formKey,
+            child: ListView(
+              children: [
+                const SizedBox(
+                  height: 75,
+                ),
+                Image.asset(
+                  'assets/images/scholar.png',
+                  height: 100,
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Tech Chat',
+                      style: TextStyle(
+                        fontSize: 32,
+                        color: Colors.white,
+                        fontFamily: 'Pacifico',
+                      ),
                     ),
-                  ),
-                ],
-              ),
-              const SizedBox(
-                height: 50,
-              ),
-              const Row(
-                children: [
-                  Text(
-                    'Register',
-                    style: TextStyle(
-                      fontSize: 24,
-                      color: Colors.white,
+                  ],
+                ),
+                const SizedBox(
+                  height: 50,
+                ),
+                const Row(
+                  children: [
+                    Text(
+                      'Register',
+                      style: TextStyle(
+                        fontSize: 24,
+                        color: Colors.white,
+                      ),
                     ),
-                  ),
-                ],
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              CustomTextField(
-                onchange: (data) {
-                  email = data;
-                },
-                labelText: 'Email',
-                hintText: 'Enter your email',
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              CustomTextField(
-                onchange: (data) {
-                  password = data;
-                },
-                labelText: 'Password',
-                hintText: 'Enter your password',
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              CustomButton(
-                ontap: () async {
-                  if (formKey.currentState!.validate()) {
-                    try {
-                      await registerUser();
-                    } on FirebaseAuthException catch (e) {
-                      if (e.code == 'weak-password') {
-                        showSnakBarMessage(context, 'Weak Password');
-                      } else if (e.code == 'email-already-in-use') {
+                  ],
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                CustomTextField(
+                  onchange: (data) {
+                    email = data;
+                  },
+                  labelText: 'Email',
+                  hintText: 'Enter your email',
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                CustomTextField(
+                  onchange: (data) {
+                    password = data;
+                  },
+                  labelText: 'Password',
+                  hintText: 'Enter your password',
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                CustomButton(
+                  ontap: () async {
+                    if (formKey.currentState!.validate()) {
+                      isloading = true;
+                      setState(() {});
+                      try {
+                        await registerUser();
+                        showSnakBarMessage(context, 'Done Succesfully');
+                      } on FirebaseAuthException catch (e) {
+                        if (e.code == 'weak-password') {
+                          showSnakBarMessage(context, 'Weak Password');
+                        } else if (e.code == 'email-already-in-use') {
+                          showSnakBarMessage(
+                              context, 'This email is already exist');
+                        }
+                      } catch (e) {
                         showSnakBarMessage(
-                            context, 'This email is already exist');
+                            context, 'There was an error, try again later');
                       }
-                    } catch (e) {
-                      showSnakBarMessage(
-                          context, 'There was an error, try again later');
-                    }
-                    showSnakBarMessage(context, 'Done Succesfully');
-                  } else {}
-                },
-                txt: 'REGISTER',
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Text(
-                    'Already Have An Account ?',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.pop(context);
-                    },
-                    child: const Text(
-                      '  Login',
-                      style: TextStyle(color: Color(0xffC7EDE6)),
+                      isloading = false;
+                      setState(() {});
+                    } else {}
+                  },
+                  txt: 'REGISTER',
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      'Already Have An Account ?',
+                      style: TextStyle(color: Colors.white),
                     ),
-                  ),
-                ],
-              ),
-              const SizedBox(
-                height: 150,
-              ),
-            ],
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.pop(context);
+                      },
+                      child: const Text(
+                        '  Login',
+                        style: TextStyle(color: Color(0xffC7EDE6)),
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 150,
+                ),
+              ],
+            ),
           ),
         ),
       ),
