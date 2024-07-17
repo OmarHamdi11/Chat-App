2038fb1a694bed7eaa925c7c79b21802fdde5d9b
OmarHamd11 <omarellafy1@gmail.com>
2024-07-17 06:00:13 +0300

    blocListner

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
+import 'package:chatapp/cubits/login_cubit/login_cubit.dart';
 import 'package:chatapp/pages/chat_page.dart';
 import 'package:chatapp/pages/login_page.dart';
 import 'package:chatapp/pages/register_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'firebase_options.dart';
 
 void main() async {
@@ -18,14 +20,17 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      routes: {
-        LoginPage.id: (context) => const LoginPage(),
-        RegisterPage.id: (context) => RegisterPage(),
-        ChatPage.id: (context) => ChatPage(),
-      },
-      debugShowCheckedModeBanner: false,
-      initialRoute: LoginPage.id,
+    return BlocProvider(
+      create: (context) => LoginCubit(),
+      child: MaterialApp(
+        routes: {
+          LoginPage.id: (context) => LoginPage(),
+          RegisterPage.id: (context) => RegisterPage(),
+          ChatPage.id: (context) => ChatPage(),
+        },
+        debugShowCheckedModeBanner: false,
+        initialRoute: LoginPage.id,
+      ),
     );
   }
 }
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:chatapp/constants.dart';
+import 'package:chatapp/cubits/login_cubit/login_cubit.dart';
 import 'package:chatapp/helpers/show_snak_bar_message.dart';
 import 'package:chatapp/pages/chat_page.dart';
 import 'package:chatapp/pages/register_page.dart';
@@ -6,151 +7,152 @@ import 'package:chatapp/widgets/custom_button.dart';
 import 'package:chatapp/widgets/custom_text_field.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
-class LoginPage extends StatefulWidget {
-  const LoginPage({super.key});
-
-  static String id = 'LoginPage';
-
-  @override
-  State<LoginPage> createState() => _LoginPageState();
-}
-
-class _LoginPageState extends State<LoginPage> {
+class LoginPage extends StatelessWidget {
   String? email;
-
   String? password;
-
   bool isloading = false;
+  static String id = 'LoginPage';
 
   GlobalKey<FormState> formKey = GlobalKey();
 
   @override
   Widget build(BuildContext context) {
-    return ModalProgressHUD(
-      inAsyncCall: isloading,
-      child: Scaffold(
-        backgroundColor: kPrimaryColor,
-        body: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 8),
-          child: Form(
-            key: formKey,
-            child: ListView(
-              children: [
-                const SizedBox(
-                  height: 75,
-                ),
-                Image.asset(
-                  'assets/images/scholar.png',
-                  height: 100,
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                const Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      'Tech Chat',
-                      style: TextStyle(
-                        fontSize: 32,
-                        color: Colors.white,
-                        fontFamily: 'Pacifico',
+    return BlocListener<LoginCubit, LoginState>(
+      listener: (context, state) {
+        if (state is LoginLoading) {
+          isloading = true;
+        } else if (state is LoginSuccess) {
+          Navigator.pushNamed(context, ChatPage.id);
+        } else if (state is LoginFailure) {
+          showSnakBarMessage(context, 'SomeThing Went Wrong');
+        }
+      },
+      child: ModalProgressHUD(
+        inAsyncCall: isloading,
+        child: Scaffold(
+          backgroundColor: kPrimaryColor,
+          body: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8),
+            child: Form(
+              key: formKey,
+              child: ListView(
+                children: [
+                  const SizedBox(
+                    height: 75,
+                  ),
+                  Image.asset(
+                    'assets/images/scholar.png',
+                    height: 100,
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  const Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Tech Chat',
+                        style: TextStyle(
+                          fontSize: 32,
+                          color: Colors.white,
+                          fontFamily: 'Pacifico',
+                        ),
                       ),
-                    ),
-                  ],
-                ),
-                const SizedBox(
-                  height: 50,
-                ),
-                const Row(
-                  children: [
-                    Text(
-                      'Login',
-                      style: TextStyle(
-                        fontSize: 24,
-                        color: Colors.white,
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 50,
+                  ),
+                  const Row(
+                    children: [
+                      Text(
+                        'Login',
+                        style: TextStyle(
+                          fontSize: 24,
+                          color: Colors.white,
+                        ),
                       ),
-                    ),
-                  ],
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                CustomTextFormField(
-                  onchange: (data) {
-                    email = data;
-                  },
-                  labelText: 'Email',
-                  hintText: 'Enter your email',
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                CustomTextFormField(
-                  onchange: (data) {
-                    password = data;
-                  },
-                  obsecure: true,
-                  labelText: 'Password',
-                  hintText: 'Enter your password',
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                CustomButton(
-                  ontap: () async {
-                    if (formKey.currentState!.validate()) {
-                      isloading = true;
-                      setState(() {});
-                      try {
-                        await signInUser();
-                        Navigator.pushNamed(context, ChatPage.id,
-                            arguments: email);
-                      } on FirebaseAuthException catch (e) {
-                        if (e.code == 'user-not-found') {
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  CustomTextFormField(
+                    onchange: (data) {
+                      email = data;
+                    },
+                    labelText: 'Email',
+                    hintText: 'Enter your email',
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  CustomTextFormField(
+                    onchange: (data) {
+                      password = data;
+                    },
+                    obsecure: true,
+                    labelText: 'Password',
+                    hintText: 'Enter your password',
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  CustomButton(
+                    ontap: () async {
+                      if (formKey.currentState!.validate()) {
+                        isloading = true;
+
+                        try {
+                          await signInUser();
+                          Navigator.pushNamed(context, ChatPage.id,
+                              arguments: email);
+                        } on FirebaseAuthException catch (e) {
+                          if (e.code == 'user-not-found') {
+                            showSnakBarMessage(
+                                context, 'No user found for that email.');
+                          } else if (e.code == 'wrong-password') {
+                            showSnakBarMessage(context,
+                                'Wrong password provided for that user.');
+                          }
+                        } catch (e) {
                           showSnakBarMessage(
-                              context, 'No user found for that email.');
-                        } else if (e.code == 'wrong-password') {
-                          showSnakBarMessage(context,
-                              'Wrong password provided for that user.');
+                              context, 'There was an error, try again later');
                         }
-                      } catch (e) {
-                        showSnakBarMessage(
-                            context, 'There was an error, try again later');
+                        isloading = false;
                       }
-                      isloading = false;
-                      setState(() {});
-                    }
-                  },
-                  txt: 'LOGIN',
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Text(
-                      'Don\'t have an account ?',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                    GestureDetector(
-                      onTap: () {
-                        Navigator.pushNamed(context, RegisterPage.id);
-                      },
-                      child: const Text(
-                        '  Register',
-                        style: TextStyle(color: Color(0xffC7EDE6)),
+                    },
+                    txt: 'LOGIN',
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text(
+                        'Don\'t have an account ?',
+                        style: TextStyle(color: Colors.white),
                       ),
-                    ),
-                  ],
-                ),
-                const SizedBox(
-                  height: 150,
-                ),
-              ],
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.pushNamed(context, RegisterPage.id);
+                        },
+                        child: const Text(
+                          '  Register',
+                          style: TextStyle(color: Color(0xffC7EDE6)),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 150,
+                  ),
+                ],
+              ),
             ),
           ),
         ),
